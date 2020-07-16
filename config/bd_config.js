7b5421aef6a6eeebd7a6072a7091bb39cf02ac28
LAPTOP-D1I711DA\Lenovo 330S <nicolascarrasco98@hotmail.com>
2020-07-15 21:13:10 -0500

    Configurar BD

--- config/bd_config.js
+++ config/bd_config.js
@@ -10,9 +10,9 @@ config = {
 const pool = new Pool({
     host: 'ec2-54-161-208-31.compute-1.amazonaws.com',
     port: 5432,
-    user: 'mrkegxewhirzat',
-    password: 'c5af009f01440e63f1353b5c10b87b4dc8b60fe05fe5376ca498c92a085cc296',
-    database: 'dsrnhaee12a4a',
+    user: 'hrtidddsbywysa',
+    password: 'fcf775d2a98b8101d678e90e31609d6e7a5a3bf0ea737fa72d4e6f7c0ab97e27',
+    database: 'dbeu6t89ksufg',
     ssl: {
         rejectUnauthorized: false
     }
